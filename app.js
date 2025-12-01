f46b46a02e9c6f9a884eef847e111f4909078e63
marcorotunjanu <marcoro04@gmail.co>
2025-12-01 10:54:04 +0200

    version 1.2
    - created endpoints for the views in my frontend and a file in which I save the list of recipes
    
    I implemented for the moment
    just a handling for GET request.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var cors = require('cors')
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-let recipesRouter = require('./routes/recipes');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recipesRouter = require('./routes/recipes');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,43 +1,70 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require("node:fs");
+const router = express.Router();
 
-function getMyData() {
-    const fs = require('node:fs');
+function getRecipesList() {
     try {
-        const data = fs.readFileSync('./recipes.json', 'utf8');
-        console.log(data);
-        return data;
+        return fs.readFileSync('./recipes.json', 'utf8');
     } catch (err) {
         console.error(err);
     }
 }
 
+function writeUpdatedRecipesList(recipe) {
+    try {
+        let recipesList = JSON.parse(fs.readFileSync('./recipes.json', 'utf8'));
+        // console.log(recipesList);
+        recipesList.push(recipe)
+        // console.log(recipesList)
+        fs.writeFileSync('./recipes.json',JSON.stringify(recipesList,null, 2),'utf-8');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function getRecipeById(id) {
+    let recipesList = JSON.parse(fs.readFileSync('./recipes.json', 'utf8'));
+    for (let recipe in recipesList) {
+        if (recipesList[recipe].id === id) {
+            return recipesList[recipe]
+        }
+    }
+}
+
+/* get all recipes */
 router.get('/', function (req, res) {
-    console.log('test recipes');
-    console.log(req.query);
-    let myrecipefromfile = JSON.parse(getMyData())
-    console.log(myrecipefromfile)
-    let myrecipe = {
-        id: '33',
-        patient: {
-            name: 'Marco',
-        },
-        doctor: {
-            name: 'Dani',
-            specialization: 'X',
-        },
-        recipe_duration: '',
-            distance_between_prescriptions: '23',
-            future_prescription_date: '12',
-            current_prescription_date: '66',
-            last_prescription_dates: '13',
-            status: 'ttttt',
-    }
-    let myrsp = {
-        status:true,rsp:myrecipefromfile
-    }
-    res.send({status:true,rsp:myrecipefromfile});
-    //res.render('recipes', {})
+    let recipes_list_from_file = JSON.parse(getRecipesList());
+    res.send(recipes_list_from_file);
+})
+
+/*get recipe with id*/
+router.get('/:id', function (req, res) {
+    let id = req.params.id
+    console.log(id)
+    let searched_recipe = getRecipeById(id)
+    res.send({result: searched_recipe})
+})
+/* post new resource - recipe*/
+router.post('/new',function(req,res){
+    const recipe = req.body;
+    console.log(recipe);
+    writeUpdatedRecipesList(recipe)
+    res.send({status:true,rsp:"Am salvat lista de retete cu success!"});
+})
+
+/*delete recipe*/
+router.delete('/:id',function(req,res){
+    let id = req.params.id;
+    console.log(id)
+    res.send({result:id})
+})
+
+/*edit recipe details*/
+router.put('/:id',function(req,res){
+    let id = req.params.id;
+    let recipe = req.body;
+    console.log(recipe)
+    res.send({result:true})
 })
 
 module.exports = router;
