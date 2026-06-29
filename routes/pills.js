7a0e68ba09bee6dce59bb6ea48355e56a3f6c404
marcorotunjanu <marcoro04@gmail.co>
2026-06-29 23:43:04 +0300

    final version

--- routes/pills.js
+++ routes/pills.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const {SerialPort, ReadlineParser} = require("serialport");
+const {readSheet} = require("read-excel-file/node");
+const fs = require("node:fs");
+const {Client} = require("pg");
 const router = express.Router();
+
+const client = new Client({
+    host: 'localhost',
+    user: 'marcorotunjanu',
+    port: 5432,
+    password: '291104',
+    database: 'casa-fabian',
+});
+
+client.connect()
 
 let boardConnected = true
 
-const port = new SerialPort({path: "/dev/cu.usbmodem11101", baudRate: 9600},(error) =>
+const port = new SerialPort({path: "/dev/cu.usbmodem1401", baudRate: 9600},(error) =>
 {
     if (error) {
         console.log('Error: ', error.message)
@@ -15,6 +28,67 @@ const parser = new ReadlineParser();
 
 port.pipe(parser)
 
+
+/*get pills configuration for patient*/
+async function getPillsConfigurationFromDB(prescription_id){
+    // console.log(prescription_id);
+    const select_prescription_configuration = {
+        text:`SELECT * FROM prescription_pills WHERE id = '${prescription_id}'`,
+    }
+    const res = await client.query(select_prescription_configuration);
+    console.log(res.rows);
+    return res.rows;
+}
+
+/*extract the digits out of the pills distribution string. An array of 20 digits is obtained.*/
+function extractPillsFromPrescriptionRow(pills_configuration_list){
+    let configuration = "";
+    let pills_and_pills_quantity_morning = pills_configuration_list[0].morning.split("#");
+    for (let i = 0; i < pills_and_pills_quantity_morning.length; i++) {
+        let pill_quantity_morning = pills_and_pills_quantity_morning[i].split(":");
+        configuration += pill_quantity_morning[1];
+    }
+    for (let i = 0; i < 5 - pills_and_pills_quantity_morning.length; i++) {
+        configuration += "0";
+    }
+
+    let pills_and_pills_quantity_lunch = pills_configuration_list[0].lunch.split("#");
+    for (let i = 0; i < pills_and_pills_quantity_lunch.length; i++) {
+        let pill_quantity_lunch = pills_and_pills_quantity_lunch[i].split(":");
+        configuration += pill_quantity_lunch[1];
+    }
+
+    for (let i = 0; i < 5 - pills_and_pills_quantity_morning.length; i++) {
+        configuration += "0";
+    }
+
+    let pills_and_pills_quantity_dinner = pills_configuration_list[0].dinner.split("#");
+    for (let i = 0; i < pills_and_pills_quantity_dinner.length; i++) {
+        let pill_quantity_dinner = pills_and_pills_quantity_dinner[i].split(":");
+        configuration += pill_quantity_dinner[1];
+    }
+
+    for (let i = 0; i < 5 - pills_and_pills_quantity_morning.length; i++) {
+        configuration += "0";
+    }
+
+    let pills_and_pills_quantity_before_bed = pills_configuration_list[0].before_bed.split("#");
+    for (let i = 0; i < pills_and_pills_quantity_before_bed.length; i++) {
+        let pill_quantity_before_bed = pills_and_pills_quantity_before_bed[i].split(":");
+        configuration += pill_quantity_before_bed[1];
+    }
+
+    for (let i = 0; i < 5 - pills_and_pills_quantity_morning.length; i++) {
+        configuration += "0";
+    }
+
+    console.log(configuration);
+    return configuration;
+}
+
+
+/*This is the function responsible for sending the request from the frontend further to the prototype.
+* It opens a connection, and write on the port the test type of the array that will be used for dispensing. */
 function arduinoResponse(test_type) {
     return new Promise((resolve,reject) => {
         port.on("open", () => {
@@ -28,8 +102,6 @@ function arduinoResponse(test_type) {
         parser.on("data",function (data){
             resolve(data)
         })
-        // parser.on("error",(err) => {
-        //daca nu primesc nimic de la arduino in 25 de secunde, se trimite mesajul "no response" (in momentul de fata, se trimite indiferent daca am sau nu un raspuns
         setTimeout(function(err){
             reject("no response");
         },25000);
@@ -38,7 +110,36 @@ function arduinoResponse(test_type) {
     })
 }
 
-router.get('/:test_type',async (req, res) => {
+/*this function is used to extract in a JSON file all the pills that are comercialized in Romania from the NOMENCLATOR*/
+async function readXLSFile(){
+    const data = await readSheet('./nomenclator.xlsx')
+    let pills_lst = []
+    for(let i=1; i < data.length; i++){
+        let pill = {
+            id:'',
+            name:'',
+        }
+        pill.id = data[i][0]
+        pill.name = data[i][1]
+        pills_lst.push(pill)
+    }
+    try{
+        fs.writeFileSync('./pills.json',JSON.stringify(pills_lst,null, 2),'utf-8');
+    }catch(err){
+        console.log(err);
+    }
+}
+
+function getPillsFromJSON(){
+    try {
+        return JSON.parse(fs.readFileSync('./pills.json', 'utf8'))
+    } catch (err) {
+        console.error(err);
+    }
+}
+//readXLSFile();
+
+router.get('/test/:test_type',async (req, res) => {
     if (!boardConnected){
         res.send({arduino: false, result:'board not connected'});
     }else{
@@ -51,12 +152,40 @@ router.get('/:test_type',async (req, res) => {
     }
 })
 
+/* sort pills for patient*/
+router.get('/sort/:id',async (req, res) => {
+    let id = req.params.id;
+    let pills = getPillsConfigurationFromDB(id)
+        .then((recipe_pills) => {
+            res.send({result: extractPillsFromPrescriptionRow(recipe_pills)});
+    })
+
+})
+
+/* sort pills for patient*/
+router.get('/pills_configuration/:id',async (req, res) => {
+    let id = req.params.id;
+    console.log(id);
+    let pills = getPillsConfigurationFromDB(id)
+        .then((recipe_pills) => {
+            res.send({result: recipe_pills});
+        })
+
+})
+
+
 router.get('/connect',async (req, res) => {
     if (!boardConnected){
         res.send({arduino: false});
     }else{
         res.send({arduino: true});
     }
+})
+
+/*get the pills extracted in the JSON file from the NOMENCLATOR*/
+router.get('/', async (req, res) => {
+    let pills_list = getPillsFromJSON();
+    res.send({list: pills_list});
 })
 
 
--- routes/recipes.js
+++ routes/recipes.js
@@ -18,12 +18,13 @@ client.connect()
 (these are dynamic properties that change often - that's why they are not saved) */
 function calculate_r_properties_for_JSON(recipe){
     let new_recipe = {id: '', patient: {name: ''}, doctor: {name: '', specialization: ''}, future_prescription_date: '', recipe_duration: '',
-        prescription_dates: [], status: '', distance_between_prescriptions: ''};
+        prescription_dates: [], status: '', distance_between_prescriptions: '', pills_list:[]};
     new_recipe.id = recipe.id;
     new_recipe.patient = recipe.patient;
     new_recipe.doctor = recipe.doctor;
     new_recipe.recipe_duration = recipe.recipe_duration;
     new_recipe.prescription_dates = recipe.prescription_dates;
+    new_recipe.pills_list = recipe.pills_list;
 
     let recipe_period = new_recipe.recipe_duration.split(' ')
     new_recipe.future_prescription_date = new Date(Date.parse(new_recipe.prescription_dates[new_recipe.prescription_dates.length - 1]) + recipe_period[0] * 30 * 24 * 60 * 60 * 1000)
@@ -42,13 +43,14 @@ function calculate_r_properties_for_JSON(recipe){
 
 function calculate_r_properties_for_DB(recipe){
     let new_recipe = {id: '', patient: {name: ''}, doctor: {name: '', specialization: ''}, future_prescription_date: '', recipe_duration: '',
-        prescription_dates: [], status: '', distance_between_prescriptions: ''};
+        prescription_dates: [], status: '', distance_between_prescriptions: '',pills_list:[]};
     new_recipe.id = recipe.id;
     new_recipe.patient.name = recipe.patient_name;
     new_recipe.doctor.name = recipe.doctor_name;
     new_recipe.doctor.specialization = recipe.doctor_specialization;
     new_recipe.recipe_duration = recipe.duration;
     new_recipe.prescription_dates = JSON.parse(recipe.prescription_dates);
+    new_recipe.pills_list = recipe.pills_list;
 
     let recipe_period = new_recipe.recipe_duration.split(' ')
     new_recipe.future_prescription_date = new Date(Date.parse(new_recipe.prescription_dates[new_recipe.prescription_dates.length - 1]) + recipe_period[0] * 30 * 24 * 60 * 60 * 1000)
@@ -103,12 +105,56 @@ function writeUpdatedRecipesListInJSON(recipe) {
     }
 }
 
+/*compute for each part of the date the pills distribution: pill_id:quantity#another_pill_id:quantity....*/
+function computePillsPrescriptionString(pills_list){
+    let pills_configuration = {
+        morning: '',
+        lunch:'',
+        dinner:'',
+        before_bed:'',
+    }
+
+    for (let pill_index = 0; pill_index < pills_list.length; pill_index++)
+    {
+        if (pill_index !== pills_list.length - 1)
+        {
+            pills_configuration.morning += pills_list[pill_index].id + ":" + pills_list[pill_index].morning + "#"
+            pills_configuration.lunch += pills_list[pill_index].id + ":" + pills_list[pill_index].lunch + "#"
+            pills_configuration.dinner += pills_list[pill_index].id + ":" + pills_list[pill_index].dinner + "#"
+            pills_configuration.before_bed += pills_list[pill_index].id + ":" + pills_list[pill_index].before_bed + "#"
+        }
+        else
+        {
+            pills_configuration.morning += pills_list[pill_index].id + ":" + pills_list[pill_index].morning
+            pills_configuration.lunch += pills_list[pill_index].id + ":" + pills_list[pill_index].lunch
+            pills_configuration.dinner += pills_list[pill_index].id + ":" + pills_list[pill_index].dinner
+            pills_configuration.before_bed += pills_list[pill_index].id + ":" + pills_list[pill_index].before_bed
+        }
+    }
+    // compute every property like : code_medicament: numar # code_medicament: numar ....
+    return pills_configuration
+}
+
 /*add new recipe to DB*/
-function addNewRecipeToDB(recipe) {
-    const insert_query = {
+async function addNewRecipeToDB(recipe) {
+    // console.log(recipe.pills_list)
+    const insert_prescriptions = {
         text: `INSERT INTO prescriptions (id, duration, prescription_dates, patient_name, doctor_name, doctor_specialization) VALUES ('${recipe.id}', '${recipe.recipe_duration}', '${JSON.stringify(recipe.prescription_dates)}', '${recipe.patient.name}', '${recipe.doctor.name}','${recipe.doctor.specialization}')`,
     }
-    return client.query(insert_query)
+
+    await client.query(insert_prescriptions);
+
+    // console.log(res)
+
+    let pills_configuration = computePillsPrescriptionString(recipe.pills_list)
+    console.log(recipe.name)
+    const insert_prescription_pills = {
+        text: `INSERT INTO prescription_pills (id, morning, lunch, dinner, before_bed) VALUES ('${recipe.id}','${pills_configuration.morning}', '${pills_configuration.lunch}', '${pills_configuration.dinner}', '${pills_configuration.before_bed}')`,
+    }
+
+    await client.query(insert_prescription_pills);
+
+    return true
 }
 
 /*edit recipe details in JSON*/
@@ -126,19 +172,33 @@ function writeUpdatedRecipe(r) {
     }
 }
 
-function updateRecipeInDB(r) {
-    const query = {
+async function updateRecipeInDB(r) {
+    const update_prescriptions_table = {
         text:`UPDATE prescriptions 
               SET id = '${r.id}', duration = '${r.recipe_duration}', 
                   prescription_dates = '${JSON.stringify(r.prescription_dates)}', patient_name = '${r.patient.name}', 
                   doctor_name = '${r.doctor.name}', doctor_specialization = '${r.doctor.specialization}'
-              WHERE id = '${r.id}'`,
+              WHERE id = '${r.id}';`,
     }
-    client.query(query,(error) => {
+    await client.query(update_prescriptions_table,(error) => {
         if (error) {
             console.error(error);
         }
     })
+    const update_prescription_pills_table = {
+        text:`UPDATE prescription_pills 
+              SET id = '${r.id}', morning = '${r.pills_distribution.morning}', 
+                  lunch = '${r.pills_distribution.lunch}', dinner = '${r.pills_distribution.dinner}', 
+                  before_bed = '${r.pills_distribution.before_bed}'
+              WHERE id = '${r.id}';`,
+    }
+    await client.query(update_prescription_pills_table,(error) => {
+        if (error) {
+            console.error(error);
+        }
+    })
+
+    return true
 }
 
 /*delete recipe*/
@@ -162,11 +222,21 @@ function deleteRecipeByIdJSON(id) {
 }
 
 /*delete recipe in DB*/
-function deleteRecipeInDB(id) {
+async function deleteRecipeInDB(id) {
     const query = {
         text: `DELETE FROM prescriptions WHERE id = '${id}'`
     }
-    client.query(query,(error) => {
+
+    await client.query(query,(error) => {
+        if (error) {
+            console.error(error);
+        }
+    })
+    const query1 = {
+        text: `DELETE FROM prescription_pills WHERE id = '${id}'`
+    }
+
+    await client.query(query1,(error) => {
         if (error) {
             console.error(error);
         }
@@ -175,18 +245,15 @@ function deleteRecipeInDB(id) {
 
 /* get all recipes */
 router.get('/', function (req, res) {
-    // let recipes_list_with_complete_properties = getRecipesListFromJSON();
-    // res.send({list : recipes_list_with_complete_properties});
     getRecipesFromDB().then((recipes_list) => {
         res.send({list: recipes_list});
     })
 })
 
+
 /* post new resource - recipe*/
 router.post('/new',function(req,res){
     const recipe = req.body;
-    // console.log(recipe);
-    // writeUpdatedRecipesListInJSON(recipe)
     addNewRecipeToDB(recipe).then((result)=>{
         if (result){
             res.send({status:true,rsp:"Am salvat lista de retete cu success!"});
@@ -196,34 +263,29 @@ router.post('/new',function(req,res){
     }).catch((err)=>{
         res.send({status:false,rsp:"insertion query function failed"});
     })
-    //     res.send({status:true,rsp:"Am salvat lista de retete cu success!"});
-    //     // res.status(200).json({
-    //     //     message: 'Recipe added successfully',
-    //     // })
-    // }).catch((err) => {
-    //     res.status(303).json({
-    //         message: 'Error occurred: ' + err.message,
-    //     })
-    // })
 })
 
 /*delete recipe*/
 router.delete('/:id',function(req,res){
     let id = req.params.id;
-    // console.log(id);
-    // deleteRecipeByIdJSON(id)
     deleteRecipeInDB(id)
     res.send({result:true})
 })
 
 /*edit recipe details*/
 router.put('/:id',function(req,res){
-    // let id = req.params.id;
     let recipe = req.body;
-    // console.log("id:" + recipe.id)
-    // writeUpdatedRecipe(recipe)
-    updateRecipeInDB(recipe)
-    res.send({result:true})
+    console.log(req.body);
+    updateRecipeInDB(recipe).then((result)=>{
+        console.log("result: " + result);
+        if (result){
+            res.send({status:true,rsp:"Am actualizat lista de retete cu success!"});
+        }else{
+            res.send({status:false,rsp:"Update failed"});
+        }
+    }).catch((err)=>{
+        res.send({status:false,rsp:"update query function failed"});
+    })
 })
 
 // module.exports = Client;
